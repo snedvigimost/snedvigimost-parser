0ed303ef0982d5baf27f8f3c01989d440665efb1
Andriy Orehov <andriyorehov@gmail.com>
2020-05-23 18:22:37 +0300

    correct ListingEntity type

--- src/entity/listing.entity.ts
+++ src/entity/listing.entity.ts
@@ -2,14 +2,43 @@ import "reflect-metadata";
 import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity({
-    name: 'listings_listing'
+  name: 'listings_listing'
 })
 export class ListingEntity {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column("text")
-    title: string;
+  @Column("text")
+  title: string;
 
+  @Column("text")
+  address: string;
+
+  @Column("int")
+  year: number;
+
+  @Column("text")
+  description: string;
+
+  @Column("int")
+  price: number;
+
+  @Column("int")
+  rooms_count: number;
+
+  @Column("int")
+  total_area: number;
+
+  @Column("int")
+  living_area: number;
+
+  @Column("int")
+  kitchen_area: number;
+
+  @Column("int")
+  floor: number;
+
+  @Column("int")
+  floor_in_house: number;
 }
